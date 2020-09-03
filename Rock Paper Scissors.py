a34e5b2ad76e5e81b9430e0881ed5af76e9f2181
swati227 <69225493+swati227@users.noreply.github.com>
2020-09-04 03:43:01 +0530

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,23 +1,24 @@
-print("Let's play Rock Paper Scissors!")
+print("Let's play rock paper scissors!")
 
 
 def rps_game():
     p1_choice = input("p1 Enter the choice: ")
     p2_choice = input("p2 Enter the choice: ")
-    if (p1_choice == "Rock" and p2_choice == "Scissors") or (p1_choice == "Scissors" and p2_choice == "Paper") or (p1_choice == "Paper" and p2_choice == "Rock"):
+
+    if (p1_choice == "rock" and p2_choice == "scissors") or (p1_choice == "scissors" and p2_choice == "paper") or (p1_choice == "paper" and p2_choice == "rock"):
         print("p1 wins!")
 
-    elif (p2_choice == "Rock" and p1_choice == "Scissors") or (p2_choice == "Scissors" and p1_choice == "Paper") or (p2_choice == "Paper" and p1_choice == "Rock"):
+    elif (p2_choice == "rock" and p1_choice == "scissors") or (p2_choice == "scissors" and p1_choice == "paper") or (p2_choice == "paper" and p1_choice == "rock"):
         print("p2 wins!")
 
     elif p1_choice == p2_choice:
         print("Match Draw! Try Again")
     else:
-        print("Invalid input! only Rock, Paper, Scissors, allowed as inputs.")
+        print("Invalid input! only rock, paper, scissors, allowed as inputs.")
 
     def play_again():
         response = input("Do you wanna play again? ")
-        if response == "Yes" or "yes" or "Y" or "y":
+        if response in ["yes", "Yes", "Y", "y"]:
             rps_game()
         else:
             print("Goodbye!")
